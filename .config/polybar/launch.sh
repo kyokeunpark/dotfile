35fb97eaf1c9c7df32cfccd80620e33c7b90b858
KyoKeun Park <kyokeunpark@gmail.com>
2018-09-10 18:15:41 -0400

    added some basic files and started writing README.md

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar intances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar Bar -c ~/.config/polybar/config &
+polybar Bar2 -c ~/.config/polybar/config &
+
+echo "Bars launched.."
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# i3_arch_rice
+# i3 & Arch Rice
 
 My personal dotfiles for Arch + i3 setup
+
+## Programs used
+
+- i3
+- polybar
+- scrot
+- zsh
+- vim
+
+## TODOs
+
+- [ ] Complete i3 config file
+- [x] Complete polybar config file 
+- [ ] Complete zsh config file 
+- [ ] Complete vimrc
+- [ ] Install ranger and modify config file 
